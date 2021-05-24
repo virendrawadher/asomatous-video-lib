2b4ea3c5ba13578cc801ed10c868cca9c14af952
virendrawadher <wadher32@gmail.com>
2021-05-25 00:01:11 +0530

    playlist provider

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { VideoListingProvider } from '../src/context/videocontextprovider';
-import {BrowserRouter as Router} from "react-router-dom"
+import { BrowserRouter as Router } from 'react-router-dom';
+import { PlayListProvider } from './context/playlist.context';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <VideoListingProvider>
-        <App />
-      </VideoListingProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router>
+			<VideoListingProvider>
+				<PlayListProvider>
+					<App />
+				</PlayListProvider>
+			</VideoListingProvider>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
